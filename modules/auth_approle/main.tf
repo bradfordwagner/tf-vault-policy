94c5ce696637df669415d6e0d9e0fa17a1987e76
bradfordwagner <wagner.bradford@gmail.com>
2024-04-15 23:29:22 -0400

    feat(approle): cicd_vault_policy_scaffolding

--- modules/auth_approle/main.tf
+++ modules/auth_approle/main.tf
@@ -17,3 +17,9 @@ resource "vault_approle_auth_backend_role" "gh_actions" {
   role_name      = "gh_terraform"
   token_policies = ["azure_sp_infra_reader"]
 }
+
+resource "vault_approle_auth_backend_role" "vault_policy_scaffolding" {
+  backend        = vault_auth_backend.approle.path
+  role_name      = "vault_policy_scaffolding"
+  token_policies = ["admin", "azure_sp_infra_reader"]
+}
