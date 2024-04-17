8aaeaa6b5abb6ea3759ab9b22697ea644c1b736e
bradfordwagner <wagner.bradford@gmail.com>
2024-04-16 22:17:33 -0400

    docs(kv2): description

--- modules/secrets/main.tf
+++ modules/secrets/main.tf
@@ -1,5 +1,5 @@
 resource "vault_mount" "kv2" {
   path        = "secret"
   type        = "kv-v2"
-  description = "This is an example KV Version 2 secret engine mount"
+  description = "static artisinally crafted secrets"
 }
